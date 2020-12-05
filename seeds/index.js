557f23ca5212904eba3aec9c531b503c46a6094f
Axel Ongaro <axel.ongaro.pro@gmail.com>
2020-12-05 03:15:16 +0100

    Third commit nothing special

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
+const campground = require("../models/campground");
 const Campground = require("../models/campground");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -14,10 +17,20 @@ db.once("open", function () {
   console.log("Database connected");
 });
 
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
 const seedDB = async () => {
   await Campground.deleteMany({});
-  const c = new Campground({ title: " purple field" });
-  await c.save();
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      location: `${cities[random1000].city},${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+    });
+    await camp.save();
+  }
 };
 
-seedDB();
+seedDB().then(() => {
+  mongoose.connection.close();
+});

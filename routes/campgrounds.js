86f395b0428cfe20ddd9127d8e1a8a1e7abb472a
Axel <axel.ongaro.pro@gmail.com>
2020-12-23 16:32:53 +0100

    feat: add part of the file upload functionnalities

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,18 +4,27 @@ const campgroundsCtrlr = require("../controllers/campgounds-ctrlr");
 const catchAsync = require("../utilities/catchAsync");
 const Campground = require("../models/campground");
 const { isAuthor, isLoggedIn, validateCampground } = require("../middleware");
+const { storage } = require("../cloudinary");
+const multer = require("multer");
+const upload = multer({ storage });
 
 router
   .route("/")
   .get(catchAsync(campgroundsCtrlr.index))
-  .post(isLoggedIn, validateCampground, catchAsync(campgroundsCtrlr.postNewForm));
+  // .post(isLoggedIn, validateCampground, catchAsync(campgroundsCtrlr.postNewForm));
+  .post(upload.array("image"), (req, res) => res.send(req.body, req.file));
 
 router.get("/new", isLoggedIn, campgroundsCtrlr.renderNewForm);
 
 router
   .route("/:id")
   .get(catchAsync(campgroundsCtrlr.showCampgrounds))
-  .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgroundsCtrlr.putEditForm))
+  .put(
+    isLoggedIn,
+    isAuthor,
+    validateCampground,
+    catchAsync(campgroundsCtrlr.putEditForm)
+  )
   .delete(isLoggedIn, isAuthor, catchAsync(campgroundsCtrlr.deleteCampground));
 
 router.get(
--- seeds/index.js
+++ seeds/index.js
@@ -25,7 +25,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
-      author: "5fd965013e1a880598853e80",
+      author: "5fe3588481627a32a378a10f",
       location: `${cities[random1000].city},${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: "https://source.unsplash.com/collection/483251",
